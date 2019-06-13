7a175b61770460e890836942d7d4d0cb58387ba4
Harathi Korrapati <korrapati.harathi@gmail.com>
2019-06-13 16:39:58 -0500

    Add files via upload

--- Nodes_Code.R
+++ Nodes_Code.R
@@ -9,10 +9,11 @@ library(tidyr)
 library(openxlsx)
 library(gsubfn)
 
-# Set working directory
-setwd("E:/Harathi data/HarathiWork_Automation/automation/Nodes")
-# Set path to directory
-filepath <- setwd("E:/Harathi data/HarathiWork_Automation/automation/Nodes")
+# Set file path
+(WD <- getwd())
+if (!is.null(WD)) setwd(WD)
+filepath <- WD
+
 # Get list of all subfolders 
 folders <- list.dirs(path=filepath, full.names=TRUE, recursive=TRUE)
 # Remove the parent folder from list
@@ -45,7 +46,7 @@ for (i in 1:length(inputFolders)) {
                                  summarise(max = max(used_percent)))
     maxDisk <- (as.data.frame(spread(maxDisk, key = host, value = max)))
     # Create a new folder for outputs
-    diskFolder <- "E:/Harathi data/HarathiWork_Automation/automation/Nodes/OUTPUTS/disk_Output/"
+    diskFolder <- paste0(WD, "/OUTPUTS/disk_Output/")
     dir.create(diskFolder, showWarnings = FALSE, recursive = TRUE) 
     # Write the outputs to excel file
     write.xlsx(maxDisk, file = paste0(diskFolder, filename, "", "_Disk_Output",".xlsx",sep = ''))
@@ -57,7 +58,7 @@ for (i in 1:length(inputFolders)) {
     
     maxMem <- (as.data.frame(spread(maxMem, key = host, value = max)))
     # Create a new folder for outputs
-    memFolder <- "E:/Harathi data/HarathiWork_Automation/automation/Nodes/OUTPUTS/Memory_Output/"
+    memFolder <- paste0(WD, "/OUTPUTS/Memory_Output/")
     dir.create(memFolder, showWarnings = FALSE, recursive = TRUE) 
     # Write the outputs to excel file
     write.xlsx(maxMem, file = paste0(memFolder, filename, "_Memory_Output",".xlsx",sep = ''))  
@@ -68,7 +69,7 @@ for (i in 1:length(inputFolders)) {
                                 summarise(max = max(load1)))
       maxCpu <- (as.data.frame(spread(maxCpu, key = host, value = max)))
       # Create a new folder for outputs
-      cpuFolder <- "E:/Harathi data/HarathiWork_Automation/automation/Nodes/OUTPUTS/Cpu_Output/"
+      cpuFolder <- paste0(WD, "/OUTPUTS/Cpu_Output/")
       dir.create(cpuFolder, showWarnings = FALSE, recursive = TRUE) 
       list_of_datasets <- list("maximum Disk" = maxCpu)
       # Write the outputs to excel file
